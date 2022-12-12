af78814aad7ae0f2473a2cba2de0a83171bb4444
d3j1x <119496452+d3j1x@users.noreply.github.com>
2022-12-12 09:47:19 +0100

    Update 02Text2.dart

--- Flutter Learn & Examples/Learn/02Text2.dart
+++ Flutter Learn & Examples/Learn/02Text2.dart
@@ -8,27 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Login(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(),
-      body: Text("Smrt Mrkt",
-          style: TextStyle(
-              fontSize: 50,
-              color: Color(0xFF021aee),
-              letterSpacing: 10.0,
-              wordSpacing: 10.0,
-              decoration: TextDecoration.underline)),
-    );
-  }
-}
 
 class Login extends StatelessWidget {
   @override
@@ -55,3 +39,22 @@ class Login extends StatelessWidget {
     );
   }
 }
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      body: Text("Smrt Mrkt",
+          style: TextStyle(
+              fontSize: 50,
+              color: Color(0xFF021aee),
+              letterSpacing: 10.0,
+              wordSpacing: 10.0,
+              decoration: TextDecoration.underline)),
+    );
+  }
+}
+
